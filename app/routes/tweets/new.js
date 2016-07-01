123dab569b6c99f384aae043f7e64d4890e6816b
Jarrett Drouillard <jarrett@thestyl.us>
2016-07-01 01:21:41 -0400

    chore: add willTransition action

--- app/routes/tweets/new.js
+++ app/routes/tweets/new.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord( 'tweet' );
+  },
+
+  actions: {
+    willTransition( transition ) {
+      if ( !confirm( 'You have unsaved changes. Do you wish to proceed?' ) && this.currentModel.get( 'hasDirtyAttributes' ) ) {
+        transition.abort();
+      } else {
+        this.currentModel.destroyRecord().then( () => {
+          return true;
+        });
+      }
+    }
   }
 });
