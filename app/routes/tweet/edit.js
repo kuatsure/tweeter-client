11c6e7af2b176ddd1b6f73242f5476960c0c1e58
Jarrett Drouillard <jarrett@thestyl.us>
2016-07-01 09:01:25 -0400

    feat: can edit / destroy / view single tweet now

--- app/routes/tweet/edit.js
+++ app/routes/tweet/edit.js
@@ -3,5 +3,46 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.modelFor( 'tweet' );
+  },
+
+  setupController( controller, model ) {
+    this._super( controller, model );
+
+    controller.set( 'scheduled', new Date( model.get( 'scheduled' ) ) );
+  },
+
+  actions: {
+    willTransition( transition ) {
+      if ( !confirm( 'You have unsaved changes. Do you wish to proceed?' ) && this.currentModel.get( 'hasDirtyAttributes' ) ) {
+        transition.abort();
+      } else {
+        return true;
+      }
+    },
+
+    submit() {
+      const m = this.currentModel;
+      const d = new Date( this.controller.get( 'scheduled' ) );
+
+      m.set( 'scheduled', d );
+
+      m.save().then( r => {
+        this.transitionTo( 'tweet', r );
+      });
+    },
+
+    cancel() {
+      this.transitionTo( 'tweet', this.currentModel );
+    },
+
+    destroy() {
+      const m = this.currentModel;
+
+      if ( confirm( 'This cannot be undone.  Do you wish to proceed?' ) ){
+        m.destroyRecord().then( () => {
+          this.transitionTo( 'tweets' );
+        });
+      }
+    }
   }
 });
