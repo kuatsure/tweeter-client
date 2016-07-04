e13dae607c25f1d18311a048b737f319ddfa882d
Jarrett Drouillard <jarrett@thestyl.us>
2016-07-03 21:54:03 -0400

    chore: quick sign-in form

--- app/routes/sign-in.js
+++ app/routes/sign-in.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    submit() {
+      return true;
+    },
+
+    cancel() {
+      this.transitionTo( 'tweets' );
+    }
+  }
 });
