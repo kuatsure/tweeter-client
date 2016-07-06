abc9133cd1f7ea8b2c38d2f0c6802f55ee7bd271
Jarrett Drouillard <jarrett@thestyl.us>
2016-07-06 11:59:04 -0400

    feat: serializerzzz

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,16 @@
 import RESTSerializer from 'ember-data/serializers/rest';
 
 export default RESTSerializer.extend({
+  normalizeFindRecordResponse( store, type, payload ) {
+    return {
+      data: {
+        id: payload.id,
+        type: type.modelName,
+        attributes: {
+          name: payload.name,
+          picture: payload.picture
+        }
+      }
+    };
+  }
 });
